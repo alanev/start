19ff6c13999e0ff754b680407308609fe53f1499
alanev <e@alanev.ru>
2016-04-05 00:39:50 +0300

    Auto import css of created modules to the main stylesheet

--- tasks/modules.js
+++ tasks/modules.js
@@ -2,6 +2,7 @@
 var fs = require('fs');
 var paths = require('./paths');
 var path = require('path');
+var glob = require('glob');
 
 var tmpls = {
 	html: String(fs.readFileSync(__dirname + '/htm.tmpl')),
@@ -20,22 +21,28 @@ var task = function () {
 			for (var key in modules) {
 				var dir = path.join(paths.modules, key);
 				if (!fs.existsSync(dir)) {
-                    
-                    var settings = modules[key];
-                    
+
+					var settings = modules[key];
+
 					fs.mkdirSync(dir);
-                    if (settings.js === true) {
-					   fs.writeFileSync(path.join(dir, key + '.js'), tmpls.js.replace('{{name}}', key));
-                    }
-                    if (settings.issues != false && !/^(g-|u-)/.test(key)) {
-					   fs.writeFileSync(path.join(dir, key + '.issues.md'), tmpls.issues.replace('{{name}}', key));
-                    }
-                    if (settings.html != false) {
-					   fs.writeFileSync(path.join(dir, key + '.htm'), tmpls.html);
-                    }
-                    if (settings.css != false) {
-					   fs.writeFileSync(path.join(dir, key + '.scss'), tmpls.css.replace('{{name}}', key));
-                    }
+					if (settings.js === true) {
+						fs.writeFileSync(path.join(dir, key + '.js'), tmpls.js.replace(/{{name}}/g, key));
+					}
+					if (settings.issues != false && !/^(g-|u-)/.test(key)) {
+						fs.writeFileSync(path.join(dir, key + '.issues.md'), tmpls.issues.replace(/{{name}}/g, key));
+					}
+					if (settings.html != false) {
+						fs.writeFileSync(path.join(dir, key + '.htm'), tmpls.html);
+					}
+					if (settings.css != false) {
+						fs.writeFileSync(path.join(dir, key + '.scss'), tmpls.css.replace(/{{name}}/g, key));
+
+						var stylesPath = glob.sync(`${paths.src}*.css`)[0];
+						if (fs.existsSync(stylesPath)) {
+							var styles = `${fs.readFileSync(stylesPath)}\n@import \'${key}\';`;
+							fs.writeFileSync(stylesPath, styles);
+						}
+					}
 				}
 			}
 		}
