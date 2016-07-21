ab8f95a63036026841c7c0c1f4c75f314cab9c99
alanev <e@alanev.ru>
2016-07-21 15:39:07 +0300

    Improoved postcss config
    + Added plugins (postcss-property-shorthand, postcss-if-media, postcss-each, postcss-animation, postcss-brand-colors)

--- tasks/css.js
+++ tasks/css.js
@@ -10,8 +10,8 @@ var config = require('../config'),
 	path = require('path'),
 	glob = require('glob')
 
-postcss = require('gulp-postcss'),
-	syntax = require('postcss-scss'),
+	postcss = require('gulp-postcss'),
+	parser = require('postcss-scss'),
 	plugins = [
 		
 		require('postcss-import')({
@@ -21,15 +21,20 @@ postcss = require('gulp-postcss'),
 				return id;
 			}
 		}),
+
+		require('postcss-property-shorthand')({
+			syntax: 'scss'
+		}),
 		
-		// Sass
+		require('postcss-if-media')(),
 		require('postcss-mixins')(),
 		require('postcss-nested')(),
 		require('postcss-extend')(),
 		require('postcss-simple-vars')(),
+		require('postcss-each')(),
 		
 		// Future css
-		require('postcss-autoreset')(),
+		// require('postcss-autoreset')(),
 		require('postcss-cssnext')(),
 		
 		// Helpers (shortcuts)
@@ -43,6 +48,10 @@ postcss = require('gulp-postcss'),
 			webpClass: ['.', config.modernizr.classPrefix, 'webp'].join(''),
 			noWebpClass: ['.', config.modernizr.classPrefix, 'no-webp'].join(''),
 		}),
+
+		// Libs
+		require('postcss-animation')(),
+		require('postcss-brand-colors')(),
 		
 		// Optimisations
 		require('css-mqpacker')(),
@@ -56,9 +65,7 @@ postcss = require('gulp-postcss'),
 var task = function() {
 	gulp.src(`${paths.src}*.css`)
 		.pipe(plumber(beep))
-		.pipe(postcss(plugins, {
-			syntax: syntax
-		}))
+		.pipe(postcss(plugins, {parser}))
 		.pipe(plumber.stop())
 		.pipe(gulp.dest(paths.dest))
 		.pipe(connect.reload());
