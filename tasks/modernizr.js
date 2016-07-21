3955f3cd920965ecae570509d45bf761156258d0
alanev <e@alanev.ru>
2016-07-21 15:34:31 +0300

    Changed modificator's separator from `_` to `--`

--- tasks/modernizr.js
+++ tasks/modernizr.js
@@ -4,7 +4,7 @@ var modernizr = require('modernizr'),
 
 var task = function () {
 	var config = require('../config').modernizr;
-	var name = config.classPrefix.replace(/_$/, '');
+	var name = config.classPrefix.replace(/--$/, '');
 	modernizr.build(config, function (result) {
 		fs.writeFile(`${paths.modules}${name}/${name}.js`, result);
 	});
